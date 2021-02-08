2ed9ee92af61ca420f00fb5b6cfc38f6d6aebfde
Yellow May <kwerep09@yahoo.com>
2021-02-08 01:02:03 +0100

    Added WhatsApp Link

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Switch, Route } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Paper from "@material-ui/core/Paper";
+import Fab from "@material-ui/core/Fab";
+import WhatsApp from "@material-ui/icons/WhatsApp";
 import Header from "./components/Header";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
@@ -11,13 +13,25 @@ function App() {
 	return (
 		<>
 			<CssBaseline />
-			<Paper>
+			<Paper style={{ position: "relative" }}>
 				<Header />
 				<Switch>
 					<Route exact path='/' component={Home} />
 					<Route path='/about' component={About} />
 					<Route path='/service' component={Service} />
 				</Switch>
+				<a href='https://wa.link/1krx15'>
+					<Fab
+						color='primary'
+						aria-label='add'
+						style={{
+							position: "fixed",
+							bottom: "1.5%",
+							right: "1.5%",
+						}}>
+						<WhatsApp />
+					</Fab>
+				</a>
 				<Footer />
 			</Paper>
 		</>
